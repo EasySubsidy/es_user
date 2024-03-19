60e0562efaf0b68ddffa36786dfee8639a2715d6
satowo619 <satodor1001@gmail.com>
2024-03-19 18:24:17 +0900

    add auth

--- app/ui/Header/index.tsx
+++ app/ui/Header/index.tsx
@@ -1,18 +1,10 @@
 import Image from "next/image";
-import Link from "next/link";
 
 import "./header.css";
-import { useAuthContext } from "@/app/feature/auth/provider/AuthProvider";
-import { FC, useEffect, useState } from "react";
-import { User, getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "@/app/lib/firebase/firebase";
+import { useAuth } from "@/app/context";
 
-type HeaderProps = {
-  user: User | null;
-};
-
-export const Header: FC<HeaderProps> = (props) => {
-  const { user } = props;
+export const Header = () => {
+  const { currentUser } = useAuth();
 
   const goToPage = (path: string) => {
     window.location.href = path;
@@ -46,7 +38,7 @@ export const Header: FC<HeaderProps> = (props) => {
       "
       >
         {/* ユーザー情報が存在する場合、ユーザーネームを表示 */}
-        {user ? (
+        {currentUser ? (
           <p
             style={{
               fontSize: 16,
@@ -55,7 +47,7 @@ export const Header: FC<HeaderProps> = (props) => {
               textAlign: "center",
             }}
           >
-            {user.email} <br />
+            {currentUser.email} <br />
             さんようこそ
           </p>
         ) : (
@@ -63,7 +55,7 @@ export const Header: FC<HeaderProps> = (props) => {
           <button
             className="header-button"
             onClick={() => {
-              goToPage("/pages/login");
+              goToPage("/login");
             }}
           >
             {/* <Link href="/pages/login"> */}
