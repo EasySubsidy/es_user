a6d96aaeea520d3fdf52c2a5e3e3e85dd3858939
satowo619 <satodor1001@gmail.com>
2024-03-24 01:11:43 +0900

    fix router

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { paths } from "./consts/paths";
+
 export default function Home() {
-  return <div className="bg-white">Home</div>;
+  const router = useRouter();
+  useEffect(() => {
+    router.push(paths.home);
+  }, []);
+
+  return <div className="bg-white h-screen"></div>;
 }
