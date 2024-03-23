70a650dbd919dca6cafdb0d1a02ff3739ea93264
takumi <103293932+takumi8420@users.noreply.github.com>
2024-03-23 15:14:07 +0900

    fix error

--- app/context/tenantContext.tsx
+++ app/context/tenantContext.tsx
@@ -5,7 +5,7 @@ import {
   useState,
   useContext,
 } from "react";
-import { Tenant, getAllTenants } from "../api/SearchTenant";
+import { Tenant, getAllTenants } from "../api/searchTenant";
 
 type TenantContextType = {
   tenants: Tenant[];
